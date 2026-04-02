5edd742626b4e7d286983dfaad2e00c91a133def
Albin Svahlstedt <albin@svahlstedt.com>
2026-04-02 17:42:50 +0200

    fix

--- src/main/java/se/su/ovning1/Book.java
+++ src/main/java/se/su/ovning1/Book.java
@@ -15,13 +15,10 @@ public class Book extends Item implements PriceableWithVAT6 {
         this.author = author;
     }
 
-    public String getType() {
-        return "Book";
-    }
+//    private String getType() {
+//        return "Book";
+//    }
 
-    public String getAuthor() {
-        return author;
-    }
 
     public double getPrice() {
 
