fdce8f3887ce793d2422e8a21ff991a23ace71ae
Godlycatpictures <leoon06@outlook.com>
2026-04-02 17:19:07 +0200

    recording

--- src/main/java/se/su/ovning1/Recording.java
+++ src/main/java/se/su/ovning1/Recording.java
@@ -1,5 +1,44 @@
 package se.su.ovning1;
 
-public class Recording {
-    
+abstract public class Recording extends Item implements PriceableWithVAT25 {
+
+    private String name;
+    private String artist;
+    private int year;
+    private int condition; // mellan 0-10
+    private double price;
+
+    public Recording(String name, String artist, int year, int condition, double price){
+        super(name); // som albin sa, "skickar vidare namnet till Item"
+        this.artist = artist;
+        this.year = year;
+        this.condition = condition;
+        this.price = price;
+    }
+
+    public abstract String getType();
+
+    public String getName(){
+        return name;
+    }
+    public String getArtist(){
+        return artist;
+    }
+    public int getYear(){
+        return year;
+    }
+    public int getCondition(){
+        return condition;
+    }
+    public double getPrice(){
+        double newPrice = price * (condition / 10.0);
+        
+        if (newPrice < 10)
+            newPrice = 10;
+
+        return newPrice * 1.25;
+    }
+    public double getOriginalPrice(){
+        return price;
+    }
 }
