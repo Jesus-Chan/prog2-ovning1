542b2282c57aefec33490a519515fd7e13a9eb54
Kannibal Gebis <rexmutter4@gmail.com>
2026-04-02 17:19:33 +0200

    CompactDisc och LongPlay

--- src/main/java/se/su/ovning1/LongPlay.java
+++ src/main/java/se/su/ovning1/LongPlay.java
@@ -1,5 +1,21 @@
 package se.su.ovning1;
 
-public class LongPlay {
-    
+import java.time.Year;
+
+public class LongPlay extends Recording {
+
+    public LongPlay(String name, String artist, int year, int condition, double price) {
+        super(name, artist, year, condition, price);
+    }
+
+    public String getType() {
+        return "LP";
+    }
+
+    public double getPrice() {
+        double basePrice = super.getPrice();
+        int currentYear = Year.now().getValue();
+        int age = currentYear - getYear();
+        return basePrice + (age * 5);
+    }
 }
