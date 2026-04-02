92ea7cebe530026ff21e584d2c60f800165c5381
Albin Svahlstedt <albin@svahlstedt.com>
2026-04-02 14:43:10 +0200

    Class files and Item

---
+++ src/main/java/se/su/ovning1/Item.java
@@ -0,0 +1,14 @@
+package se.su.ovning1;
+
+public abstract class Item {
+    
+    protected String name;
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
